f776b00888f5c218c0ed9fda2dd70dcc467be155
App Academy Student <student@appacademy.io>
2015-05-19 15:52:00 -0700

    Create Session class to read and update session cookie

--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -6,17 +6,32 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      session_cookie = req.cookies.find do |cookie|
+        cookie.name == cookie_name
+      end
+      if session_cookie
+        @content = JSON.parse(session_cookie.value)
+      else
+        @content = {}
+      end
     end
 
     def [](key)
+      @content[key]
     end
 
     def []=(key, val)
+      @content[key] = val
+    end
+
+    def cookie_name
+      '_rails_lite_app'
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new(cookie_name, @content.to_json)
     end
   end
 end
