d85fb7399bde3fdd95596aa597afd3fdb5a63860
App Academy Student <student@appacademy.io>
2015-05-19 15:50:27 -0700

    Add rendering methods to ControllerBase

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -7,7 +7,16 @@ module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
+    def controller_name
+      self.class.name.underscore
+    end
+
     def render(template_name)
+      template_path = "views/#{controller_name}/#{template_name}.html.erb"
+
+      template_string = File.read(template_path)
+      template = ERB.new(template_string)
+      render_content(template.result(binding), 'text/html')
     end
   end
 end
